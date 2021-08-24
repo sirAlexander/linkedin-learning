888a90b380b576f44c2af8bff677d4f83387f91d
sirAlexander <alexander.shikanga.tindi@gmail.com>
2021-08-24 14:10:41 +0100

    call order client from user service

--- java-microservices-gRPC/shopping-services/src/main/java/com/shopping/service/UserServiceImpl.java
+++ java-microservices-gRPC/shopping-services/src/main/java/com/shopping/service/UserServiceImpl.java
@@ -1,14 +1,25 @@
 package com.shopping.service;
 
+import com.shopping.client.OrderClient;
 import com.shopping.db.User;
 import com.shopping.db.UserDao;
+import com.shopping.stubs.order.Order;
 import com.shopping.stubs.user.Gender;
 import com.shopping.stubs.user.UserRequest;
 import com.shopping.stubs.user.UserResponse;
 import com.shopping.stubs.user.UserServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
+
+    private static final Logger logger = Logger.getLogger(UserServiceImpl.class.getName());
 
     private UserDao userDao = new UserDao();
 
@@ -23,9 +34,31 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
                 .setName(user.getName())
                 .setAge(user.getAge())
                 .setGender(Gender.valueOf(user.getGender()));
+
+        List<Order> orders = getOrders(userResponseBuilder);
+
+        userResponseBuilder.setNoOfOrders(orders.size());
         final UserResponse userResponse = userResponseBuilder.build();
 
         responseObserver.onNext(userResponse);
         responseObserver.onCompleted();
+    }
+
+    private List<Order> getOrders(UserResponse.Builder userResponseBuilder) {
+        // get orders by invoking the order client
+        ManagedChannel channel = ManagedChannelBuilder
+                .forTarget("localhost:50052")
+                .usePlaintext()
+                .build();
+
+        OrderClient orderClient = new OrderClient(channel);
+        List<Order> orders = orderClient.getOrders(userResponseBuilder.getId());
+
+        try {
+            channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException ex) {
+            logger.log(Level.SEVERE, "Channel did not shutdown", ex);;
+        }
+        return orders;
     }
 }
