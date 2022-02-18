046f2a106905f8a67f0d9888d28bb54446e55e08
Alexander Shikanga-Tindi <alexander.tindi@yapily.com>
2022-02-18 18:37:31 +0000

    getting started with spring batch

---
+++ linkedin-batch-01-04/src/test/java/com/linkedin/batch/LinkedinBatch0104ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinBatch0104ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
